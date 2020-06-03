1c110d2dbc21e4852c012e23ce64b29fd0e095cc
Mustafin Dinis <tavsheng@yandex.ru>
2020-06-03 22:23:34 +0500

    Updates

---
+++ precache-manifest.a90346974938f76c174dc69bfd581197.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d72e4ad27bb20a9494ccd6060035b08a",
+    "url": "/tavsheng/myReact/index.html"
+  },
+  {
+    "revision": "b9d93afd96f1f517466f",
+    "url": "/tavsheng/myReact/static/css/3.6ee78432.chunk.css"
+  },
+  {
+    "revision": "a4c4b6f3eb5b6b24b49e",
+    "url": "/tavsheng/myReact/static/css/4.8a236e41.chunk.css"
+  },
+  {
+    "revision": "34aca3332438b196a368",
+    "url": "/tavsheng/myReact/static/css/main.c61d1836.chunk.css"
+  },
+  {
+    "revision": "75049be87441aed13fd4",
+    "url": "/tavsheng/myReact/static/js/2.c7185e09.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/tavsheng/myReact/static/js/2.c7185e09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9d93afd96f1f517466f",
+    "url": "/tavsheng/myReact/static/js/3.a2e00d76.chunk.js"
+  },
+  {
+    "revision": "a4c4b6f3eb5b6b24b49e",
+    "url": "/tavsheng/myReact/static/js/4.9edb5ada.chunk.js"
+  },
+  {
+    "revision": "34aca3332438b196a368",
+    "url": "/tavsheng/myReact/static/js/main.52bc730c.chunk.js"
+  },
+  {
+    "revision": "a85a219155d7950da03a",
+    "url": "/tavsheng/myReact/static/js/runtime-main.78573802.js"
+  },
+  {
+    "revision": "9064981b80e59f1b663c17fab41563c9",
+    "url": "/tavsheng/myReact/static/media/preloader.9064981b.svg"
+  }
+]);
